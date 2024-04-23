c9f053dfe344fba5b998d31f3539e422e06afbb8
CrazyProger1 <crazyproger1@gmail.com>
2024-04-23 20:59:27 +0300

    Refactor: client

--- resty/middlewares/manager.py
+++ resty/middlewares/manager.py
@@ -19,15 +19,15 @@ class MiddlewareManager(BaseMiddlewareManager):
 
         self._middlewares = []
 
-    async def call_request_middlewares(self, request: Request, **kwargs):
+    async def call_request_middlewares(self, request: Request, **context):
         for middleware in self._middlewares:
             if isinstance(middleware, BaseRequestMiddleware):
-                await middleware.handle_request(request=request, **kwargs)
+                await middleware.handle_request(request=request, **context)
 
-    async def call_response_middlewares(self, response: Response, **kwargs):
+    async def call_response_middlewares(self, response: Response, **context):
         for middleware in self._middlewares:
             if isinstance(middleware, BaseResponseMiddleware):
-                await middleware.handle_response(response=response, **kwargs)
+                await middleware.handle_response(response=response, **context)
 
     def add_middleware(self, middleware: BaseMiddleware):
         if not isinstance(middleware, BaseMiddleware):
